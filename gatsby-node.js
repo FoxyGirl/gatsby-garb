bc0dfbc4d391bb5df5c020047817f06fa345b2f2
Elena Pavlova <lenlho22@gmail.com>
2019-05-11 17:14:31 +0300

    Created blog with pagination using blog-template

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
 const PostTemplate = path.resolve('./src/templates/post-template.js')
+const BlogTemplate = path.resolve('./src/templates/blog-template.js')
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
@@ -40,6 +41,26 @@ exports.createPages = async ({ graphql, actions }) => {
       component: PostTemplate,
       context: {
         slug: post.fields.slug,
+      },
+    })
+  })
+
+  posts.forEach((_, index, postsArr) => {
+    const totalPages = postsArr.length
+    const postsPerPage = 1
+    const currentPage = index + 1
+    const isFirstPage = currentPage === 1
+    const isLastPage = currentPage === totalPages
+    createPage({
+      path: isFirstPage ? '/blog' : `/blog/${currentPage}`,
+      component: BlogTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: index * postsPerPage,
+        isFirstPage,
+        isLastPage,
+        currentPage,
+        totalPages,
       },
     })
   })
---
+++ src/pages/post-three.md
@@ -0,0 +1,8 @@
+---
+title: 'My Third Markdown post'
+date: '2019-03-07'
+---
+
+This is my third post made using Markdown.
+
+And I wont to say something else.
